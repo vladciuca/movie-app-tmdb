ea85688deb9ae06b836cf63cc04de9e41c2157d3
Vlad Ciuca <vladccdev@gmail.com>
2023-03-27 16:45:10 +0300

    updates

--- src/components/Error/Error.styles.js
+++ src/components/Error/Error.styles.js
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 export const ErrorContainer = styled.div`
   max-width: 1000px;
-  display: flex;
-  justify-content: center;
+  margin: 0 auto;
 `;
 
 export const ErrorMessage = styled.div`
   font-size: 1.2rem;
+  text-align: center;
 `;
